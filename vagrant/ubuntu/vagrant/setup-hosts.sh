85ee1d34e4f5970cd351d077e37c9f1181059f0a
Conall Ó Gribín <conallogribin@gmail.com>
2022-06-28 01:20:47 +0100

    Initial Commit

--- vagrant/ubuntu/vagrant/setup-hosts.sh
+++ vagrant/ubuntu/vagrant/setup-hosts.sh
@@ -10,7 +10,6 @@ sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
 # Update /etc/hosts about other hosts
 cat >> /etc/hosts <<EOF
 192.168.5.11  master-1
-192.168.5.12  master-2
 192.168.5.21  worker-1
 192.168.5.22  worker-2
 192.168.5.30  lb
